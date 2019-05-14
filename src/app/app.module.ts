37b72aefe7e3728aaa701376101517c9dcf3e8c7
Anthony Ortega <aortega1285@mail.fresnostate.edu>
2019-05-13 18:05:27 -0700

    last commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { MainHeaderComponent } from './main-header/main-header.component';
 import { MainFooterComponent } from './main-footer/main-footer.component';
@@ -14,7 +14,8 @@ import { MainContentComponent } from './main-content/main-content.component';
     MainContentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -5,6 +5,7 @@ interface IStudent {
   firstName: string;
   lastName: string;
   course: string;
+  editMode: boolean;
 }
 @Component({
   selector: 'app-main-content',
@@ -15,6 +16,7 @@ export class MainContentComponent implements OnInit {
 
 
   studentArray: Array<IStudent> = [];
+  disableAddButton = false;
   constructor() { }
 
   ngOnInit() {
@@ -23,19 +25,22 @@ export class MainContentComponent implements OnInit {
         id: 1,
         firstName: 'Anthony',
         lastName: 'Ortega',
-        course: 'Network Engineering'
+        course: 'Network Engineering',
+        editMode: false
       },
       {
         id: 2,
         firstName: 'Vanessa',
         lastName: 'Thao',
-        course: 'Child Development'
+        course: 'Child Development',
+        editMode: false
       },
       {
         id: 3,
         firstName: 'Alex',
         lastName: 'Moun',
-        course: 'Data Analytics'
+        course: 'Data Analytics',
+        editMode: false
       }
     ];
   }
@@ -43,17 +48,30 @@ export class MainContentComponent implements OnInit {
   addStudent() {
     this.studentArray.unshift(
       {
-        id: 1,
-        firstName: 'Anthony',
-        lastName: 'Ortega',
-        course: 'Network Engineering'
+        id: null,
+        firstName: null,
+        lastName: null,
+        course: null,
+        editMode: true
       }
     );
+    this.disableAddButton = true;
   }
 
   removeStudent(index: number) {
-    console.log('index', index);
     this.studentArray.splice(index, 1);
+  }
+
+  saveStudent() {
+    this.studentArray[0].editMode = false;
+    this.disableAddButton = false;
+    this.sort();
+  }
+
+  sort() {
+    this.studentArray.sort((a: IStudent , b: IStudent) => {
+      return a.id < b.id ? -1 : 1;
+    });
   }
 
 
